1ce5a00cf30395caad5f32739fab0b3e359e8618
Naeem Hasan <naeemhasan252@gmail.com>
2020-04-05 18:01:09 +0600

    complete about-me page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+//    initial all section without home display none
+    $('section').css('display','none');
+
+    $('#home').fadeIn('slow');
+
 //    navigation toggle
     $('.hamberger').on('click',function (e) {
         $('nav').css('left','0');
@@ -7,5 +12,31 @@ $(document).ready(function () {
     $('#closeNav').on('click',function () {
         $('nav').css('left','-60%');
     })
+
+//    page
+
+    $('nav ul').on('click',function (e) {
+
+        if(e.target.tagName=='A'){
+            $('nav ul li a').removeClass('active');
+
+            e.target.className+=' active';
+            let id=e.target.getAttribute('data-id');
+           $('section').css('display','none');
+
+           $('section#'+id).fadeIn('slow');
+
+
+        }
+    });
+//    nav close when width is <600 and click main element
+
+    $('main').click(function () {
+        if(window.innerWidth<=600){
+            setTimeout(function () {
+                $('nav').css('left','-60%');
+            },500)
+        }
+    });
 
 });
